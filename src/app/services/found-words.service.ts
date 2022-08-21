f30ab8577e5c4d6aa8cb576e318816690cd75aba
mjcwly <mjcwly@gmail.com>
2022-08-21 20:57:38 +0100

    Added explicit types to FoundWordService observables

--- src/app/services/found-words.service.ts
+++ src/app/services/found-words.service.ts
@@ -3,7 +3,6 @@ import { map, merge, Observable, of, Subject } from 'rxjs';
 import { tap, withLatestFrom } from 'rxjs/operators';
 import { GlobalStateService } from './global-state.service';
 import { KeyPressService } from './key-press.service';
-import { TypedLettersService } from './typed-letters.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,22 +11,25 @@ export class FoundWordService {
   private initialFoundWords$: Observable<string[]> = of([]);
 
   private resetFoundWordsSubject$ = new Subject<void>();
-  private resetFoundWords$ = this.resetFoundWordsSubject$.pipe(map(() => []));
+  private resetFoundWords$: Observable<string[]> =
+    this.resetFoundWordsSubject$.pipe(map(() => []));
 
-  private newFoundWord$ = this.keyPressService.enterKeyPress$.pipe(
-    withLatestFrom(this.globalStateService.typedLetters$),
-    map(([_, typedLetters]) => typedLetters)
-  );
+  private newFoundWord$: Observable<string> =
+    this.keyPressService.enterKeyPress$.pipe(
+      withLatestFrom(this.globalStateService.typedLetters$),
+      map(([_, typedLetters]) => typedLetters)
+    );
 
-  private accumulatedFoundWords$ = this.newFoundWord$.pipe(
-    withLatestFrom(this.globalStateService.foundWords$),
-    map(([newWord, enteredWords]) => {
-      const accumulatedWords = [...enteredWords, newWord];
-      return accumulatedWords;
-    })
-  );
+  private accumulatedFoundWords$: Observable<string[]> =
+    this.newFoundWord$.pipe(
+      withLatestFrom(this.globalStateService.foundWords$),
+      map(([newWord, enteredWords]) => {
+        const accumulatedWords = [...enteredWords, newWord];
+        return accumulatedWords;
+      })
+    );
 
-  displayFoundWords$ = merge(
+  displayFoundWords$: Observable<string[]> = merge(
     this.initialFoundWords$,
     this.accumulatedFoundWords$,
     this.resetFoundWords$
